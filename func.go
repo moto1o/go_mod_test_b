20dfeff36060ce7e9904db52402a6a9ccb9aec01
moto1o <moto1o@163.com>
2025-10-17 10:07:03 +0000

    v1.0.1

--- func.go
+++ func.go
@@ -5,5 +5,5 @@ import "github.com/moto1o/go_mod_test_a"
 
 
 func PrintDepVersion() {
-	fmt.Println("version: v1.0.0", "dep go_mod_test_a => ",go_mod_test_a.NameAndVersion())
+	fmt.Println("version: v1.0.1", "dep go_mod_test_a => ",go_mod_test_a.NameAndVersion())
 }
